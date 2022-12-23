1c90dc0ed079b3cca3ec5ce8040029ada4ac2264
Raghav Kattel <arg.ahv7@gmail.com>
2022-12-23 14:34:36 +0545

    report chart for beneficiaries

--- modules/beneficiaries/beneficiaries.model.js
+++ modules/beneficiaries/beneficiaries.model.js
@@ -3,47 +3,132 @@ const { DataTypes, Sequelize } = require("@rumsan/core").SequelizeDB;
 const { AbstractModel } = require("@rumsan/core/abstract");
 
 const schema = {
-  id: {
-   type : Sequelize.INTEGER,
-   primaryKey : true,
-   autoIncrement: true,
-   allowNull : false
-  },
-
   name: {
     type: Sequelize.STRING,
-    allowNull : false,
+    allowNull: false,
   },
 
-  gender : {
-    type : Sequelize.ENUM,
-    values : ['male', 'female', 'others']
+  gender: {
+    type: Sequelize.ENUM(["M", "F", "O", "U"]),
+    defaultValue: "U",
   },
-
-  phone : {
-    type : Sequelize.STRING
+  phone: {
+    type: Sequelize.STRING,
   },
-
-  walletAddress : {
-    type : Sequelize.STRING
+  walletAddress: {
+    type: Sequelize.STRING,
   },
-
-  cnicNumber : {
-    type : Sequelize.STRING
+  cnicNumber: {
+    type: Sequelize.STRING,
   },
+  address: {
+    type: Sequelize.JSON,
+    set(v) {
+      if (
+        !(
+          v.hasOwnProperty("taluka") &&
+          v.hasOwnProperty("district") &&
+          v.hasOwnProperty("village")
+        )
+      ) {
+        throw new Error("district, talika and village is required");
+      }
 
-  address : {
-    type : Sequelize.JSON,
-  },
-
-  extras : {
-    type : Sequelize.JSON,
+      return this.setDataValue("address", JSON.stringify(v));
     },
-
-  email : {
-    type : Sequelize.STRING,
+    get() {
+      const v = this.getDataValue("address");
+      return v ? JSON.parse(v) : {};
+    },
+    defaultValue: JSON.stringify({
+      taluka: "",
+      district: "",
+      village: "",
+    }),
+  },
+  phoneOwnedBy: {
+    type: Sequelize.STRING,
+  },
+  simRegisteredUnder: {
+    type: Sequelize.STRING,
+  },
+  phoneType: {
+    type: Sequelize.ENUM(["smartphone", "featurephone", "dumbphone"]),
+    set(v) {
+      if (v) {
+        this.setDataValue("hasPhone", true);
+      }
+      this.setDataValue("phoneType", v);
+    },
+  },
+  phoneOwnerRelation: {
+    type: Sequelize.STRING,
+  },
+  unionCouncil: {
+    type: Sequelize.STRING,
+  },
+  relationship: {
+    type: Sequelize.STRING,
+  },
+  relativeName: {
+    type: Sequelize.STRING,
+  },
+  hasInternetAccess: {
+    type: Sequelize.BOOLEAN,
+  },
+  bankAccount: {
+    type: Sequelize.STRING,
+    set(v) {
+      if (v) {
+        this.setDataValue("isBanked", true);
+      }
+      this.setDataValue("bankAccount", v);
+    },
+  },
+  isBanked: {
+    type: Sequelize.BOOLEAN,
+  },
+  hasPhone: {
+    type: Sequelize.BOOLEAN,
+  },
+  bankAccountType: {
+    type: Sequelize.ENUM(["current", "savings"]),
+  },
+  dailyDistanceCovered: {
+    type: Sequelize.STRING,
+  },
+  dailyWaterConsumption: {
+    type: Sequelize.STRING,
   },
 
+  extras: {
+    type: Sequelize.JSON,
+    set(v) {
+      v = JSON.stringify(v);
+      this.setDataValue("extras", v);
+    },
+    get() {
+      const v = this.getDataValue("extras");
+      return v ? JSON.parse(v) : {};
+    },
+    // defaultValue: JSON.stringify({
+    //   phoneOwnedBy: "",
+    //   simRegisteredUnder: "",
+    //   phoneType: "",
+    //   phoneOwnerRelation: "",
+    //   unionCouncil: "",
+    //   relationship: "",
+    //   relativeName: "",
+    //   hasInternetAccess: null,
+    //   bankAccount: "",
+    //   dailyDistanceCovered: "",
+    //   dailyWaterConsumption: "",
+    // }),
+  },
+
+  email: {
+    type: Sequelize.STRING,
+  },
 };
 module.exports = class Beneficiaries extends AbstractModel {
   schema = schema;
