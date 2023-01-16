7eb0ac566b5d96162af2bfc554d22ef1e045abd3
unknown <abiskar.rumsan@gmail.com>
2023-01-16 17:44:07 +0545

    added routes

--- modules/index.js
+++ modules/index.js
@@ -9,6 +9,7 @@ const Beneficiaries = require("./beneficiaries");
 const Projects = require("./project");
 const _Reports = require("./reporting");
 const _Auth = require("./auth");
+const _Misc = require("./misc");
 const { EVENTS } = require("../constants");
 
 let Routes = {
@@ -26,6 +27,7 @@ let Routes = {
   Beneficiaries: new Beneficiaries(),
   Projects: new Projects(),
   Reports: new _Reports(),
+  Misc: new _Misc(),
 };
 
 module.exports = Routes;
--- modules/models.js
+++ modules/models.js
@@ -3,12 +3,14 @@ const UserModel = require("./user/user.model");
 const TagModel = require("./tag/tag.model");
 const BeneficiariesModel = require("./beneficiaries/beneficiaries.model");
 const ProjectModel = require("./project/project.model");
+const MiscModel = require("./misc/misc.model");
 
 let modelFactory = {
   TagModel: new TagModel().init(),
   UserModel: new UserModel().init(),
   BeneficiariesModel: new BeneficiariesModel().init(),
- ProjectModel : new ProjectModel().init()
+  ProjectModel: new ProjectModel().init(),
+  MiscModel: new MiscModel().init(),
 };
 
 /**********************************************************
