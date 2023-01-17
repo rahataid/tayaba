bfbe17bee3ce200ffcfe2a9fa7cea811258a63f2
Raghav Kattel <arg.ahv7@gmail.com>
2023-01-17 12:51:41 +0545

    misc fix

--- modules/project/project.validator.js
+++ modules/project/project.validator.js
@@ -7,15 +7,15 @@ const validators = {
       name: Joi.string().required().error(new Error("Invalid name")),
       startDate: Joi.date().optional().error(new Error("Invalid Start Date")),
       endDate: Joi.date().optional().error(new Error("Invalid End Date")),
-      projectManager: Joi.string().optional().error(new Error("Invalid Project manager Name")),
+      projectManager: Joi.string()
+        .optional()
+        .error(new Error("Invalid Project manager Name")),
       location: Joi.string().optional().error(new Error("Invalid location")),
     }),
-
   },
   delete: {
     params: Joi.object({
       id: Joi.number().required(),
-
     }),
   },
   getById: {
@@ -27,7 +27,7 @@ const validators = {
     params: Joi.object({
       id: Joi.number().required(),
     }),
-  }
+  },
 };
 
 module.exports = class extends AbstractValidator {
