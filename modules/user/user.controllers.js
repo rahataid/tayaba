7735ed154ccbe1e1e475ec5961481dc194dc0d01
Raghav Kattel <arg.ahv7@gmail.com>
2023-01-19 14:24:59 +0545

    otp

--- modules/user/user.controllers.js
+++ modules/user/user.controllers.js
@@ -1,6 +1,12 @@
 const { UserController } = require("@rumsan/user");
 const Settings = require("../../helpers/settings");
 
+const getPrivateKeys = (file) => {
+  const keyFile = require(`../../config/privateKeys/${file}.json`);
+
+  return keyFile;
+};
+
 module.exports = class extends UserController {
   constructor(options = {}) {
     options.mixins = {
@@ -12,9 +18,14 @@ module.exports = class extends UserController {
         );
         let data = await this.loginSuccess(userId, clientIpAddress);
         console.log(data.user.roles);
-        if (data.user.roles?.includes("Tayaba")) {
-          data.privateKey = "ssssss";
+        if (data.user.roles?.includes("donor")) {
+          let keys = getPrivateKeys("donor");
+
+          data.privateKey = keys.privateKey;
         } else {
+          let keys = getPrivateKeys("admin");
+
+          data.privateKey = keys.privateKey;
         }
         return data;
       },
