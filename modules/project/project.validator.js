abd99e7379aadb6418a291bc1655743b96d8ed3c
unknown <abiskar.rumsan@gmail.com>
2023-03-24 17:03:06 +0545

    project aadd model chanege

--- modules/project/project.validator.js
+++ modules/project/project.validator.js
@@ -1,15 +1,17 @@
-const Joi = require("joi");
-const { AbstractValidator } = require("@rumsan/core/abstract");
+const Joi = require('joi');
+const { AbstractValidator } = require('@rumsan/core/abstract');
 
 const validators = {
   add: {
     payload: Joi.object({
-      name: Joi.string().required().example('H20 Wheels').error(new Error("Invalid name")),
-      startDate: Joi.date().optional().error(new Error("Invalid Start Date")),
-      endDate: Joi.date().optional().error(new Error("Invalid End Date")),
-      owner: Joi.number().example(1),
-      budget : Joi.number().required().example(0),
-      disbursed : Joi.number().required().example(0),
+      name: Joi.string().required().example('H20 Wheels').error(new Error('Invalid name')),
+      startDate: Joi.date().optional().error(new Error('Invalid Start Date')),
+      endDate: Joi.date().optional().error(new Error('Invalid End Date')),
+      projectManager: Joi.string().example('jdgfdgh'),
+      extras: Joi.object().optional(),
+      location: Joi.string().required(),
+      description: Joi.string().required(),
+      projectType: Joi.string().required(),
     }),
   },
   delete: {
