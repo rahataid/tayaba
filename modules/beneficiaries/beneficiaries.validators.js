ab6bc0fbd61c571085890b067929f453b34012aa
unknown <abiskar.rumsan@gmail.com>
2023-03-31 15:05:39 +0545

    changed beneficiary validator

--- modules/beneficiaries/beneficiaries.validators.js
+++ modules/beneficiaries/beneficiaries.validators.js
@@ -9,27 +9,16 @@ const validators = {
       cnicNumber: Joi.string().example('12334'),
       phone: Joi.string().example('456345'),
       walletAddress: Joi.string().required().example('0xABJWAN6666...'),
-      phoneOwnedBy: Joi.string().required().example('azimsd'),
-      simRegisteredUnder: Joi.string().example('azimsd'),
       phoneType: Joi.string()
         .valid('smartphone', 'featurephone', 'dumbphone')
         .example('smartphone'),
-      phoneOwnerRelation: Joi.string().example('azimsd'),
-      unionCouncil: Joi.string().example('azimsd'),
-      relationship: Joi.string().example('azimsd'),
-      relativeName: Joi.string().example('azimsd'),
-      hasInternetAccess: Joi.bool().required().example(false),
-      bankAccount: Joi.string().example('azimsd').optional(),
-      bankAccountType: Joi.valid('current', 'savings').optional().example('savings'),
-      dailyDistanceCovered: Joi.number().example(10),
-      dailyWaterConsumption: Joi.number().example(4),
-      villageId: Joi.number().example(1),
-      isBanked: Joi.bool().required().example(false),
+      phoneOwnedBy: Joi.string().required().example('SELF'),
       projectId: Joi.number().example(1),
       miscData: Joi.object({}),
+      villageId: Joi.number().example(1),
+      community: Joi.string().example('gfhdgfh'),
     }),
   },
-
   getById: {
     params: Joi.object({
       id: Joi.number(),
